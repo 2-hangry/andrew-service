cc0edcbe1d16f55d281e442dda2da2f4994b3a1d
Andrew Marine <andrewmarine7882@gmail.com>
2018-09-08 21:13:41 -0700

    Refactored data generating script
    - no longer adds extra new line at end of file
    
    - moved functions around and added notes for readability

--- dbDataGenAndSeeding/dataGenerators.js
+++ dbDataGenAndSeeding/dataGenerators.js
@@ -1,46 +1,61 @@
 const faker = require('faker');
 const fs = require('fs');
 
+// <------------------------ CSV text generating script for photo table ------------------->
+
 const makePhoto = () => (
   `${faker.random.number(100)},${faker.random.number(50)},${faker.image.food()},${faker.date.past(5)},${faker.lorem.sentence(10)},${faker.random.number(50)},false`
 );
+
+const makePhotos = () => {
+  let photosCSV = 'businessId,imageUploaderId,imageUrl,imageUploadDate,imageComment,helpfulCount,reported\n';
+  const photos = [];
+
+  for (let i = 0; i < 1000; i += 1) {
+    photos.push(makePhoto());
+  }
+  photosCSV += photos.join('\n');
+
+  return photosCSV;
+};
+
+// <------------------------ CSV text generating script for user table ------------------->
 
 const makeUser = () => (
   `${faker.name.findName()},${faker.image.avatar()},${faker.random.number(1000)},${faker.random.number(500)},${faker.random.boolean()}`
 );
 
-const makeBusiness = () => (
-  `${faker.company.companyName(0)}`
-);
-
-const makePhotos = () => {
-  let photosCSV = 'businessId,imageUploaderId,imageUrl,imageUploadDate,imageComment,helpfulCount,reported\n';
-
-  for (let i = 0; i < 1000; i += 1) {
-    photosCSV += `${makePhoto()}\n`;
-  }
-
-  return photosCSV;
-};
 const makeUsers = () => {
   let usersCSV = 'userName,profileImageUrl,friendsCount,reviewsCount,eliteStatus\n';
+  const users = [];
 
   for (let i = 0; i < 50; i += 1) {
-    usersCSV += `${makeUser()}\n`;
+    users.push(makeUser());
   }
+  usersCSV += users.join('\n');
 
   return usersCSV;
 };
 
+// <------------------------ CSV text generating script for business table ------------------->
+
+const makeBusiness = () => (
+  `${faker.company.companyName(0)}`
+);
+
 const makeBusinesses = () => {
   let businessesCSV = 'businessName\n';
+  const businesses = [];
 
   for (let i = 0; i < 100; i += 1) {
-    businessesCSV += `${makeBusiness()}\n`;
+    businesses.push(makeBusiness());
   }
+  businessesCSV += businesses.join('\n');
 
   return businessesCSV;
 };
+
+// <------------------------ file writing scripts ------------------->
 
 const photos = makePhotos();
 const users = makeUsers();
