b68e849143e1895a0edd97c35ac5c9d3d3c0bfd6
Andrew Marine <andrewmarine7882@gmail.com>
2018-09-10 23:55:42 -0700

    PhotoCarouselContainer renders 3 photos and rotates through photos on left/right click

---
+++ client/src/components/CarouselPicture.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const PictureCarousel = ({ picture, data }) => (
+  <div>
+    <h1>{picture}</h1>
+  </div>
+);
+
+export default PictureCarousel;
--- client/src/components/PhotoCarouselContainer.jsx
+++ client/src/components/PhotoCarouselContainer.jsx
@@ -1,5 +1,51 @@
-import React from 'react';
+import React, { Component } from 'react';
+import CarouselPicture from './CarouselPicture';
 
-const PhotoCarouselContainer = () => <div>Hello World</div>;
+export default class PhotoCarouselContainer extends Component {
+  constructor(props) {
+    super(props);
 
-export default PhotoCarouselContainer;
+    this.state = {
+      pictureIdx: 0,
+    };
+
+    this.handleLeftButtonClick = this.handleLeftButtonClick.bind(this);
+    this.handleRightButtonClick = this.handleRightButtonClick.bind(this);
+  }
+
+  handleLeftButtonClick() {
+    const { pictureIdx } = this.state;
+
+    if (pictureIdx > 0) {
+      this.setState({
+        pictureIdx: pictureIdx - 1,
+      });
+    }
+  }
+
+  handleRightButtonClick() {
+    const { pictureIdx } = this.state;
+    const { data } = this.props;
+
+    if (pictureIdx + 2 < data.photos.length) {
+      this.setState({
+        pictureIdx: pictureIdx + 1,
+      });
+    }
+  }
+
+  render() {
+    const { data } = this.props;
+    const { pictureIdx } = this.state;
+
+    return (
+      <div>
+        <button type="button" id="left-button" onClick={() => this.handleLeftButtonClick()} />
+        <CarouselPicture picture={pictureIdx} data={data} />
+        <CarouselPicture picture={pictureIdx + 1} data={data} />
+        <CarouselPicture picture={pictureIdx + 2} data={data} />
+        <button type="button" id="right-button" onClick={() => this.handleRightButtonClick()} />
+      </div>
+    );
+  }
+}
