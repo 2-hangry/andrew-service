e559e62fcc5b20cc50bc721caa6ed8b77f129f7d
Andrew Marine <andrewmarine7882@gmail.com>
2018-09-10 17:46:43 -0700

    updated get/post handlers
    - business id is now part of url in get and post
    
    - split post method into 2 seperate handlers for  cleaner readabiltiy

--- server/index.js
+++ server/index.js
@@ -1,32 +1,26 @@
 const express = require('express');
-const bp = require('body-parser');
 const resBuilder = require('./responseBuilders');
 const db = require('../db/controllers');
 
 const app = express();
 
-app.use(bp.json());
-app.use(bp.urlencoded({ extended: true }));
-
-app.get('/businesses/business-id/images', (req, res) => {
+app.get('/businesses/:id/images', (req, res) => {
   resBuilder
-    .buildGetResponse(req.query.id)
+    .buildGetResponse(req.params.id)
     .then(data => res.json(data))
     .catch(err => res.send(err));
 });
 
-app.post('/businesses/business-id/images', (req, res) => {
-  if (req.body.type === 'helpful') {
-    db.updateHelpfulCount(req.body.id)
-      .then(data => res.json(data))
-      .catch(err => res.send(err));
-  } else if (req.body.type === 'report') {
-    db.updateReported(req.body.id)
-      .then(data => res.json(data))
-      .catch(err => res.send(err));
-  } else {
-    res.end('Invalid request type');
-  }
+app.post('/businesses/:id/images/helpful', (req, res) => {
+  db.updateHelpfulCount(req.params.id)
+    .then(data => res.json(data))
+    .catch(err => res.send(err));
+});
+
+app.post('/businesses/:id/images/report', (req, res) => {
+  db.updateReported(req.params.id)
+    .then(data => res.json(data))
+    .catch(err => res.send(err));
 });
 
 app.listen(3000, () => console.log('listening on port 3000'));
